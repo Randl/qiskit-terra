90812200adb84f97e9a0e2817f4aae040d86b942
Evgenii Zheltonozhskii <zheltonozhskiy@gmail.com>
2023-04-16 10:38:13 +0300

    Fix transpiler mypy errors

--- qiskit/transpiler/passes/scheduling/alignments/align_measures.py
+++ qiskit/transpiler/passes/scheduling/alignments/align_measures.py
@@ -123,7 +123,9 @@ class AlignMeasures(TransformationPass):
         """
         time_unit = self.property_set["time_unit"]
 
-        if not _check_alignment_required(dag, self.alignment, Measure):
+        if not _check_alignment_required(
+            dag, self.alignment, Measure
+        ):  # TODO: should be Measure()?
             # return input as-is to avoid unnecessary scheduling.
             # because following procedure regenerate new DAGCircuit,
             # we should avoid continuing if not necessary from performance viewpoint.
